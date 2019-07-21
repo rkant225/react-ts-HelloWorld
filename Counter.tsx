6840b40758d47e1065ae909492da1ff0b924064d
rkant225 <rkant225@gmail.com>
2019-07-22 02:08:46 +0530

    Initial commit

---
+++ Counter.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+
+interface ICounterProps{
+
+}
+
+interface ICounterState{
+  count: number
+}
+export default class Counter extends React.Component<ICounterProps,ICounterState> {
+  constructor(props){
+    super(props);
+    this.state=
+    {
+      count:0
+    };
+
+  }
+
+  private increment(){
+    this.setState({count: (this.state.count + 1)});
+  }
+
+  private decrement(){
+    this.setState({ count: (this.state.count - 1)});
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    );
+  }
+}
---
+++ index.tsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+import Counter from './Counter';
+
+interface AppProps { }
+interface AppState {
+  name: string;
+}
+
+class App extends Component<AppProps, AppState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Rahul'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Hello name={this.state.name} />
+        <Counter />
+        <p>
+          Start editing to see some magic happen :)
+        </p>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
